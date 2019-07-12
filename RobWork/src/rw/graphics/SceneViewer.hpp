db813fbf9dafb82f2c2f00ba1f57bfa7b240ee91
Ken <khgs@mmmi.sdu.dk>
2019-07-12 13:35:28 +0200

    Fixed some doxygen issues.

--- RobWork/src/rw/graphics/SceneViewer.hpp
+++ RobWork/src/rw/graphics/SceneViewer.hpp
@@ -128,7 +128,14 @@ namespace graphics {
          */
         virtual rw::math::Vector3D<> getViewCenter() = 0;
 
-        //! 
+        /**
+         * @brief picks the drawable in the scene that intersects with the ray (x,y,-1) in
+         * camera coordinates.
+         *
+         * @param x [in] first camera coordinate.
+         * @param y [in] second camera coordinate.
+         * @return the picked drawable.
+         */
         virtual DrawableNode::Ptr pickDrawable(int x, int y) = 0;
         
         //! @copydoc rw::graphics::SceneGraph::pickDrawable(SceneGraph::RenderInfo&,int,int)
