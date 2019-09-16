85bc03ca9ab6c42750fa419a2463912e2ed2cfec
Kasper Høj Lorenzen <kalor@mmmi.sdu.dk>
2019-09-16 10:47:57 +0200

    Text render class

---
+++ RobWork/src/rwlibs/opengl/RenderText.hpp
@@ -0,0 +1,20 @@
+#ifndef RWLIBS_OPENGL_RENDERTEXT_HPP
+#define RWLIBS_OPENGL_RENDERTEXT_HPP
+
+/**
+ * @file RenderText.hpp
+ */
+
+#include <rwlibs/os/rwgl.hpp>
+
+namespace rwlibs { namespace opengl {
+
+    class RenderText
+    {
+    public:
+    private:
+    
+    };
+
+}}
+#endif // end include guard
