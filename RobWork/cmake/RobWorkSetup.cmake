cb02db8f806e976f358505c1922136b6a91dc284
Kasper Høj Lorenzen <kalor@mmmi.sdu.dk>
2019-10-30 14:03:15 +0100

    Set glutfound on freeglut found

--- RobWork/cmake/RobWorkSetup.cmake
+++ RobWork/cmake/RobWorkSetup.cmake
@@ -170,7 +170,12 @@ INCLUDE(CMakeDependentOption)
 set(RW_HAVE_GLUT False)
 find_package(GLUT QUIET)
 if ( NOT GLUT_FOUND)
-  find_package(FreeGLUT REQUIRED)
+  find_package(FreeGLUT QUIET)
+  if (FreeGLUT_FOUND)
+    #set(GLUT_glut_LIBRARY ${FreeGLUT})
+    set(GLUT_FOUND ${FreeGLUT_FOUND})
+  endif()
+
 endif ()
 
 if( OPENGL_FOUND AND GLUT_FOUND) 
