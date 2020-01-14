aa93777a347e6b65f4c35a231eaf367722dc7b50
Kasper Høj Lorenzen <kalor@mmmi.sdu.dk>
2020-01-14 16:19:52 +0100

    Fixed warnings

--- RobWorkSim/test/tasksimulation/GraspTaskSimulation.cpp
+++ RobWorkSim/test/tasksimulation/GraspTaskSimulation.cpp
@@ -20,7 +20,7 @@
 #include <RobWorkSimConfig.hpp>
 
 #include <boost/test/unit_test.hpp>
-#include <boost/test/floating_point_comparison.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
 
 #include <rw/loaders/path/PathLoader.hpp>
 #include <rwlibs/task/GraspTask.hpp>
