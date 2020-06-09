2e4ab2d82b1fcd37a110a064172450f4880f3375
Kasper Høj Lorenzen <kalor@mmmi.sdu.dk>
2020-06-09 06:43:54 +0000

    Transform3d cubic spline

--- RobWork/src/rw/trajectory/CubicSplineFactory.hpp
+++ RobWork/src/rw/trajectory/CubicSplineFactory.hpp
@@ -1,7 +1,7 @@
 /********************************************************************************
- * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute, 
- * Faculty of Engineering, University of Southern Denmark 
- * 
+ * Copyright 2009 The Robotics Group, The Maersk Mc-Kinney Moller Institute,
+ * Faculty of Engineering, University of Southern Denmark
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +15,6 @@
  * limitations under the License.
  ********************************************************************************/
 
-
 #ifndef RW_TRAJECTORY_CUBICSPLINEFACTORY_HPP
 #define RW_TRAJECTORY_CUBICSPLINEFACTORY_HPP
 
@@ -23,112 +22,237 @@
  * @file CubicSplineFactory.hpp
  */
 
-
 #include "InterpolatorTrajectory.hpp"
 #include "Path.hpp"
 
-namespace rw {
-namespace trajectory {
+#include <rw/math/Q.hpp>
+#include <rw/math/Transform3D.hpp>
+#include <rw/math/Transform3DVector.hpp>
 
-/** @addtogroup trajectory */
-/*@{*/
+namespace rw { namespace trajectory {
 
-/**
- * @brief Factory for creating cubic splines
- *
- *
- */
-class CubicSplineFactory
-{
-public:
+    /** @addtogroup trajectory */
+    /*@{*/
 
     /**
-     * @brief constructs a free/natural cubic spline
-     * A natural cubic spline has free boundary conditions. Only one condition
-     * can be said for the end points namely acceleration is zero.
-     * * The spline passes through each data point.
-     * * The spline forms a continuous function over [a,b].
-     * * The spline forms a smooth function.
-     * * The second derivative is continuous.
+     * @brief Factory for creating cubic splines
      *
-     * @param qpath [in] a list of points that the spline should intersect
-     * @param timeStep [in] the duration of each spline path
-     */
-	static InterpolatorTrajectory<rw::math::Q>::Ptr makeNaturalSpline(QPath::Ptr qpath, double timeStep=1.0);
-
-    /**
-     * @brief constructs a natural cubic spline, see above.
      *
-     * @param tqpath [in] a list of points with associated timestaps. The spline will intersect
-     * the points at the time specified in \b tqpath
-	 * @cond
-     * @param offset [in]
-	 * @endcond
-	 * @return a trajectory of CubicSplineInterpolators
      */
-	static InterpolatorTrajectory<rw::math::Q>::Ptr makeNaturalSpline(TimedQPath::Ptr tqpath);
+    class CubicSplineFactory
+    {
+      public:
+        /**
+         * @brief constructs a free/natural cubic spline
+         * A natural cubic spline has free boundary conditions. Only one condition
+         * can be said for the end points namely acceleration is zero.
+         * * The spline passes through each data point.
+         * * The spline forms a continuous function over [a,b].
+         * * The spline forms a smooth function.
+         * * The second derivative is continuous.
+         *
+         * @param qpath [in] a list of points that the spline should intersect
+         * @param timeStep [in] the duration of each spline path
+         */
+        static InterpolatorTrajectory< rw::math::Q >::Ptr makeNaturalSpline (QPath::Ptr qpath,
+                                                                             double timeStep = 1.0);
 
-	/**
-	 * @brief Construct a natural cubic spline. See documentation of CubicSplineFactory::makeNaturalSpline(QPath::Ptr, double)	 
-	 *
-	 * @param qpath [in] Path to follow
-	 * @param times [in] Times associated to the different configurations in \b qpath	 
-	 * @return a trajectory of CubicSplineInterpolators
-	 */
-	static InterpolatorTrajectory<rw::math::Q>::Ptr makeNaturalSpline(const QPath& qpath, const std::vector<double>& times);
+        /**
+         * @brief constructs a natural cubic spline, see above.
+         *
+         * @param tqpath [in] a list of points with associated timestaps. The spline will intersect
+         * the points at the time specified in \b tqpath
+         * @cond
+         * @param offset [in]
+         * @endcond
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Q >::Ptr
+        makeNaturalSpline (TimedQPath::Ptr tqpath);
 
+        /**
+         * @brief Construct a natural cubic spline. See documentation of
+         * CubicSplineFactory::makeNaturalSpline(QPath::Ptr, double)
+         *
+         * @param qpath [in] Path to follow
+         * @param times [in] Times associated to the different configurations in \b qpath
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Q >::Ptr
+        makeNaturalSpline (const QPath& qpath, const std::vector< double >& times);
 
-	/**
-	 * @brief creates a clamped spline trajectory with equally spaced
-	 * via points. That is time between samples is 1. A clamped spline controls
-	 * the velocity in the end points. The acceleration is 0 in the end points.
-	 * @param qpath [in] the path over which the spline should be generated.
-	 * @param dqStart [in] the velocity in the first point
-	 * @param dqEnd [in] the velocity in the last point.
-	 * @param timeStep documentation missing !
-	 * @return a trajectory of CubicSplineInterpolators
-	 */
-	static InterpolatorTrajectory<rw::math::Q>::Ptr makeClampedSpline(QPath::Ptr qpath,
-			const rw::math::Q& dqStart,
-		    const rw::math::Q& dqEnd,
-		    double timeStep = 1.0);
+        /**
+         * @brief constructs a free/natural cubic spline
+         * A natural cubic spline has free boundary conditions. Only one condition
+         * can be said for the end points namely acceleration is zero.
+         * * The spline passes through each data point.
+         * * The spline forms a continuous function over [a,b].
+         * * The spline forms a smooth function.
+         * * The second derivative is continuous.
+         *
+         * @param path [in] a list of points that the spline should intersect
+         * @param timeStep [in] the duration of each spline path
+         */
+        static InterpolatorTrajectory< rw::math::Transform3DVector<> >::Ptr
+        makeNaturalSpline (const Path< rw::math::Transform3D<> >& path, double timeStep = 1.0);
 
-	/**
-	 * @brief creates a clamped spline trajectory where the timed label is used
-	 * to determine the time between samples. A clamped spline controls
-	 * the velocity in the end points. The acceleration is 0 in the end points.
-	 * @param tqpath [in] the path over which the spline should be generated.
-	 * @param dqStart [in] the velocity in the first point
-	 * @param dqEnd [in] the velocity in the last point.
-	 * @return a trajectory of CubicSplineInterpolators
-	 */
-	static InterpolatorTrajectory<rw::math::Q>::Ptr makeClampedSpline(TimedQPath::Ptr tqpath,
-			const rw::math::Q& dqStart,
-		    const rw::math::Q& dqEnd);
+        /**
+         * @brief constructs a natural cubic spline, see above.
+         * @param path [in] a list of points with associated timestaps. The spline will intersect
+         * the points at the time specified in \b tqpath
+         * @cond
+         * @param offset [in]
+         * @endcond
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Transform3DVector<> >::Ptr
+        makeNaturalSpline (const Path< Timed< rw::math::Transform3D<> > >& path);
 
+        /**
+         * @brief Construct a natural cubic spline. See documentation of
+         * CubicSplineFactory::makeNaturalSpline(QPath::Ptr, double)
+         *
+         * @param path [in] Path to follow
+         * @param times [in] Times associated to the different configurations in \b path
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Transform3DVector<> >::Ptr
+        makeNaturalSpline (const Path< rw::math::Transform3D<> >& path,
+                           const std::vector< double >& times);
 
-	/**
-	 * @brief creates a clamped spline trajectory where the timed label is used
-	 * to determine the time between samples. A clamped spline controls
-	 * the velocity in the end points. The acceleration is 0 in the end points.
-	 * @param qpath [in] the path over which the spline should be generated.
-	 * @param times [in] the times associated to the configurations in \b qpath.
-	 * @param dqStart [in] the velocity in the first point
-	 * @param dqEnd [in] the velocity in the last point.
-	 * @return a trajectory of CubicSplineInterpolators
-	 */
-	static InterpolatorTrajectory<rw::math::Q>::Ptr makeClampedSpline(const QPath& qpath, const std::vector<double>& times, const rw::math::Q& dqStart, const rw::math::Q& dqEnd);
+        /**
+         * @brief constructs a free/natural cubic spline
+         * A natural cubic spline has free boundary conditions. Only one condition
+         * can be said for the end points namely acceleration is zero.
+         * * The spline passes through each data point.
+         * * The spline forms a continuous function over [a,b].
+         * * The spline forms a smooth function.
+         * * The second derivative is continuous.
+         *
+         * @param path [in] a list of points that the spline should intersect
+         * @param timeStep [in] the duration of each spline path
+         */
+        template< typename T >
+        static typename InterpolatorTrajectory< T >::Ptr makeNaturalSpline (const Path< T >& path,
+                                                                            double timeStep = 1.0);
 
-private:
-	CubicSplineFactory();
+        /**
+         * @brief constructs a natural cubic spline, see above.
+         * @param path [in] a list of points with associated timestaps. The spline will intersect
+         * the points at the time specified in \b tqpath
+         * @cond
+         * @param offset [in]
+         * @endcond
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        template< typename T >
+        static typename InterpolatorTrajectory< T >::Ptr
+        makeNaturalSpline (const Path< Timed< T > >& path);
 
-	virtual ~CubicSplineFactory();
-};
+        /**
+         * @brief Construct a natural cubic spline. See documentation of
+         * CubicSplineFactory::makeNaturalSpline(QPath::Ptr, double)
+         *
+         * @param path [in] Path to follow
+         * @param times [in] Times associated to the different configurations in \b path
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        template< typename T >
+        static typename InterpolatorTrajectory< T >::Ptr
+        makeNaturalSpline (const Path< T >& path, const std::vector< double >& times);
 
+        /**
+         * @brief creates a clamped spline trajectory with equally spaced
+         * via points. That is time between samples is 1. A clamped spline controls
+         * the velocity in the end points. The acceleration is 0 in the end points.
+         * @param qpath [in] the path over which the spline should be generated.
+         * @param dqStart [in] the velocity in the first point
+         * @param dqEnd [in] the velocity in the last point.
+         * @param timeStep documentation missing !
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Q >::Ptr
+        makeClampedSpline (QPath::Ptr qpath, const rw::math::Q& dqStart, const rw::math::Q& dqEnd,
+                           double timeStep = 1.0);
 
-/** @} */
+        /**
+         * @brief creates a clamped spline trajectory where the timed label is used
+         * to determine the time between samples. A clamped spline controls
+         * the velocity in the end points. The acceleration is 0 in the end points.
+         * @param tqpath [in] the path over which the spline should be generated.
+         * @param dqStart [in] the velocity in the first point
+         * @param dqEnd [in] the velocity in the last point.
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Q >::Ptr
+        makeClampedSpline (TimedQPath::Ptr tqpath, const rw::math::Q& dqStart,
+                           const rw::math::Q& dqEnd);
 
-} //end namespace trajectory
-} //end namespace rw
+        /**
+         * @brief creates a clamped spline trajectory where the timed label is used
+         * to determine the time between samples. A clamped spline controls
+         * the velocity in the end points. The acceleration is 0 in the end points.
+         * @param qpath [in] the path over which the spline should be generated.
+         * @param times [in] the times associated to the configurations in \b qpath.
+         * @param dqStart [in] the velocity in the first point
+         * @param dqEnd [in] the velocity in the last point.
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        static InterpolatorTrajectory< rw::math::Q >::Ptr
+        makeClampedSpline (const QPath& qpath, const std::vector< double >& times,
+                           const rw::math::Q& dqStart, const rw::math::Q& dqEnd);
+
+        /**
+         * @brief creates a clamped spline trajectory with equally spaced
+         * via points. That is time between samples is 1. A clamped spline controls
+         * the velocity in the end points. The acceleration is 0 in the end points.
+         * @param path [in] the path over which the spline should be generated.
+         * @param dStart [in] the velocity in the first point
+         * @param dEnd [in] the velocity in the last point.
+         * @param timeStep documentation missing !
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        template< typename T >
+        static typename InterpolatorTrajectory< T >::Ptr
+        makeClampedSpline (const Path< T >& path, const T& dStart, const T& dEnd,
+                           double timeStep = 1.0);
+
+        /**
+         * @brief creates a clamped spline trajectory where the timed label is used
+         * to determine the time between samples. A clamped spline controls
+         * the velocity in the end points. The acceleration is 0 in the end points.
+         * @param tqpath [in] the path over which the spline should be generated.
+         * @param dqStart [in] the velocity in the first point
+         * @param dqEnd [in] the velocity in the last point.
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        template< typename T >
+        static typename InterpolatorTrajectory< T >::Ptr
+        makeClampedSpline (const Path< Timed< T > >& tpath, const T& dStart, const T& dEnd);
+
+        /**
+         * @brief creates a clamped spline trajectory where the timed label is used
+         * to determine the time between samples. A clamped spline controls
+         * the velocity in the end points. The acceleration is 0 in the end points.
+         * @param qpath [in] the path over which the spline should be generated.
+         * @param times [in] the times associated to the configurations in \b qpath.
+         * @param dqStart [in] the velocity in the first point
+         * @param dqEnd [in] the velocity in the last point.
+         * @return a trajectory of CubicSplineInterpolators
+         */
+        template< typename T >
+        static typename InterpolatorTrajectory< T >::Ptr
+        makeClampedSpline (const Path< T >& path, const std::vector< double >& times,
+                           const T& dStart, const T& dEnd);
+
+      private:
+        CubicSplineFactory ();
+
+        virtual ~CubicSplineFactory ();
+    };
+
+    /** @} */
+
+}}    // namespace rw::trajectory
 
 #endif /*RW_TRAJECTORY_CUBICSPLINEFACTORY_HPP_*/
